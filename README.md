1cbe947d6678a4789c9250fd1c876f6bae7fcf77
Your Name <you@example.com>
2023-12-06 14:49:27 +0300

    добавил расчет уровня поддержки и сопротивление, вывод итоговых данных по предстказанию и расчетам

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # stock_predication
 
 Проект для предсказания цены акции на ближайшие 15 дней.
-В качестве данных используется датасет с сайта Yahho Finance.
+В качестве данных используется датасет с сайта investing.com.
+
+## Как скачать необходимый датасет?
+
+
+На примере акции SBER.
+
+Предворительно зарегистрируйтесь на сайте. https://www.investing.com. 
+
+Далее:
+
+- Откройте страницу https://www.investing.com/equities/sberbank_rts
+![Alt text](docs/images/1.PNG)
+
+#### Перейдите в разде Historical Data ![Alt text](docs/images/2.PNG)
+
+#### Скачайте необходимый датасет и поместите его в директорию ./app/content
+
+#### Исправьте в юпитер ноутбуке Stock_predication_RF_investing.ipynb путь к файлу, например:
+
+```
+
+    historical_prices = preparation(pd.read_csv("./app/content/SBER.csv"))
+
+```
+Запустите юпитер ноутбук Stock_predication_RF_investing.ipynb
+
--- app/predication.py
+++ app/predication.py
@@ -13,14 +13,46 @@ def preducation(historical_prices):
         test_size=len(closedf)-training_size
         train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
 
+        time_step = 15
+        X_train, y_train = create_dataset(train_data, time_step)
+        X_test, y_test = create_dataset(test_data, time_step)
+
+        return X_test, y_train, X_test, y_test
+
     except Exception as e:
             return f"An error occurred: {str(e)}"
 
 
-    def create_dataset(dataset, time_step=1):
+def create_dataset(dataset, time_step=1):
         dataX, dataY = [], []
         for i in range(len(dataset)-time_step-1):
             a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
             dataX.append(a)
             dataY.append(dataset[i + time_step, 0])
         return np.array(dataX), np.array(dataY)
+
+
+def randomForestregressor(X_train, y_train, X_test, y_test):
+    from sklearn.ensemble import RandomForestRegressor
+
+    regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
+    regressor.fit(X_train, y_train)
+
+    # Lets Do the prediction 
+
+    train_predict=regressor.predict(X_train)
+    test_predict=regressor.predict(X_test)
+
+    train_predict = train_predict.reshape(-1,1)
+    test_predict = test_predict.reshape(-1,1)
+
+    
+
+
+
+
+
+    
+
+
+    
